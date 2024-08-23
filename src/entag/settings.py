4619673645ca3ce785f65335c43e8c6747d52eb3
verc-oni <89543105+verc-oni@users.noreply.github.com>
2024-08-23 20:31:20 +0100

    Update settings.py

--- src/entag/settings.py
+++ src/entag/settings.py
@@ -286,7 +286,7 @@ DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-if config('DEFAULT_STATIC_CONFIG', cast=bool):
+if config('DEFAULT_STATIC_CONFIG', cast=bool, default=False):
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # else:
